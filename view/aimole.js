7a4f623dfa551e918a4efac7722d772234d8ecba
leoyuholo <leoyuholo@gmail.com>
2016-04-14 21:22:28 +0800

    add game view simulator

--- view/aimole.js
+++ view/aimole.js
@@ -2,21 +2,47 @@
 (function(global){
 	'use strict';
 
-	var parseParams = () => {
-		var hash = window.location.hash.replace(/^#/, '');
-		if (!hash) return {}
-		var params = {};
-		try{
-			params = JSON.parse(decodeURIComponent(hash));
-		} catch (err) {
-			console.error(err, 'hash: ' + hash);
+	let parseParams = () => {
+		let vars = window.location.hash.replace(/^#/, '').split('&');
+		let params = {};
+		for(let i = 0, l = vars.length; i < l; i++) {
+			if(vars[i] !== '') {
+				let pair = vars[i].split('=');
+				if(pair[0]) params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+			}
 		}
 		return params;
 	}
 
-	var aimole = parseParams();
+	let eventEmitter = () => {
+		let events = {}
+		let emitter = {
+			on: (eventName, func) => {
+				let listeners = events[eventName] = events[eventName] || [];
+				listeners.push(func);
+			},
+			off: (eventName, func) => {
+				let listeners = events[eventName] || [];
+				let index = listeners.indexOf(func);
+				if (index == -1) return;
+				listeners.splice(index, 1);
+			},
+			emit: (eventName, ...args) => {
+				let listeners = events[eventName];
+				if (!listeners || !listeners.length) return;
+				listeners.forEach(func => func(...args));
+			}
+		}
+		return emitter;
+	}
+
+	let aimole = parseParams();
 	// console.log('aimole', aimole);
-	if (aimole.display) {
+	let emitter = eventEmitter();
+	aimole.on = emitter.on;
+	aimole.off = emitter.off;
+	aimole.emit = emitter.emit;
+	if (aimole.display && !aimole.simulateStreaming) {
 		try {
 			aimole.display = JSON.parse(aimole.display);
 		} catch (err) {
@@ -24,29 +50,66 @@
 			aimole.display = [];
 		}
 		// console.log('aimole.display', aimole.display);
+	} else if (aimole.display && aimole.simulateStreaming) {
+		let display = aimole.display;
+		aimole.display = [];
+		try {
+			display = JSON.parse(display);
+		} catch (err) {
+			console.error(err, 'JSON string: ' + display);
+			display = [];
+		}
+		let events = [
+			{eventName: 'queueing'},
+			{eventName: 'start'}
+		]
+		.concat(display.map(data => {return {eventName: 'display', args: [data]};}))
+		.concat([
+			{eventName: 'end'},
+			{eventName: 'disconnect', args: ['io server disconnect']},
+		]);
+		events.forEach( (event, index) => {
+			setTimeout( () => {
+				if (event.eventName === 'display'){
+					aimole.display.push(event.args[0]);
+				}
+				if (event.args)
+					aimole.emit(event.eventName, ...event.args);
+				else
+					aimole.emit(event.eventName);
+			}, (index + 1) * 200);
+		});
+		// console.log('aimole.simulateStreaming', events);
 	} else if (aimole.streamUrl && aimole.matchId) {
 		aimole.display = [];
 		io(aimole.streamUrl, {query: 'matchId=' + aimole.matchId})
 			.on('queueing', () => {
-				// console.log('queueing');
+				aimole.emit('queueing');
 			})
 			.on('start', () => {
-				// console.log('start');
+				aimole.emit('start');
 			})
 			.on('display', (data) => {
-				// console.log('display', data);
 				aimole.display.push(data);
+				aimole.emit('display', data);
 			})
 			.on('err', (errMsg) => {
-				console.error(new Error(errMsg));
+				aimole.emit('error', new Error(errMsg));
 			})
 			.on('end', () => {
-				// console.log('end');
+				aimole.emit('end');
 			})
 			.on('disconnect', (reason) => {
-				// console.log('socket disconnected, reason: ', reason);
+				aimole.emit('disconnect', reason);
 			});
+		// console.log('aimole.streamUrl', aimole.streamUrl, 'aimole.matchId', aimole.matchId);
 	}
+
+	// aimole.on('queueing', () => console.log('queueing'));
+	// aimole.on('start', () => console.log('start'));
+	// aimole.on('display', (data) => console.log('display', data));
+	// aimole.on('end', () => console.log('end'));
+	// aimole.on('disconnect', (reason) => console.log('disconnect', reason));
 
 	// AMD support
 	if (typeof define === 'function' && define.amd) {
